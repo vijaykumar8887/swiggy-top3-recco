d74cb0cf061e969b60ee063c58ceb0b16686be5a
surya <vijaykumarbhai7379@gmail.com>
2023-12-06 18:14:46 +0530

    cookicutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project recommend top 3 best food of restaurant',
+    author='Surya',
+    license='BSD-3',
+)
